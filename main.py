eadec9b38aa3897cbcebb33e927b9b2abea52ac5
Cherrysourballs <griever.lory@gmail.com>
2026-03-13 23:10:33 +0100

    B: added more helpers, updated main.py to add a triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,6 +9,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -15,7 +21,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        milliseconds = clock.tick(60)
+        dt = milliseconds / 1000 
+    
 if __name__ == "__main__":
     main()
