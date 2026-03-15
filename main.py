fd499bbaec47e6dcb542338d4e7f1e4b152e6a37
Cherrysourballs <griever.lory@gmail.com>
2026-03-16 00:34:41 +0100

    L: made shots and asteroids destructible

--- main.py
+++ main.py
@@ -44,6 +44,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
         for i in drawable:
             i.draw(screen)
             
